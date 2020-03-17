dcabc48ea0a122d31ef43d28419130ff622cde79
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-17 21:17:26 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -16,16 +16,13 @@ function crazy() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-
   thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
   sayMy();
-
   function sayMy() {
 
     var name = "Kristin";
